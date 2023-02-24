80443f7b8d6ad264e5ce22b202919fba6ca8eff0
Josanghyeon <BStkdgus.dev@gmail.com>
2023-02-25 01:21:39 +0900

    fix: company export 삭제

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 export * from './auth/signIn';
 export * from './auth/signUp';
-export * from './company/profile';
-export * from './company/profile/list';
 export * from './user/profile';
 export * from './user/profile/write';
