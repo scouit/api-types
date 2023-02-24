b5ae831db1abe159752a4b98d704b7e0abbddbc2
ipiyou <tao710803@gmail.com>
2023-02-25 03:01:20 +0900

    fix: api type 구분

--- src/user/profile/write.ts
+++ src/user/profile/write.ts
@@ -8,39 +8,53 @@ export type QueryProfileWriteType =
   | 'education';
 
 export interface PatchProfileWriteRequestType {
-  introduce?: {
-    simple?: string;
-    complex?: string;
-  };
-  basic: {
-    role?: string;
-  };
-  technology: {
-    main?: string[];
-    sub?: string[];
-  };
-  activity?: {
-    name: string;
-    content?: string;
-    period: string;
-  }[];
-  education?: {
-    name: string;
-    period: string;
-  }[];
-  project?: {
-    name: string;
-    introduce?: string;
-    period: string;
-    url?: string[];
-    works?: string[];
-    skills: string[];
-    images?: string[];
-  }[];
-  workExperience: {
-    name: string;
-    period: string;
-    role?: string;
-    works?: string[];
-  }[];
+  introduce?: IntroduceType;
+  basic: BasicType;
+  technology: TechnologyType;
+  activity?: ActivityType[];
+  education?: EducationType[];
+  project?: ProjectType[];
+  workExperience: WorkExperienceType[];
+}
+
+export interface IntroduceType {
+  simple?: string;
+  complex?: string;
+}
+
+export interface BasicType {
+  role?: string;
+}
+
+export interface TechnologyType {
+  main?: string[];
+  sub?: string[];
+}
+
+export interface ActivityType {
+  name: string;
+  content?: string;
+  period: string;
+}
+
+export interface EducationType {
+  name: string;
+  period: string;
+}
+
+export interface ProjectType {
+  name: string;
+  introduce?: string;
+  period: string;
+  url?: string[];
+  works?: string[];
+  skills: string[];
+  images?: string[];
+}
+
+export interface WorkExperienceType {
+  name: string;
+  period: string;
+  role?: string;
+  works?: string[];
 }
